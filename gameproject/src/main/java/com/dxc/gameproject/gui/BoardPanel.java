40296b4af11e3f95b2e67daa8c704a9130764f84
kuzniar <miron.kuzniar@gmail.com>
2017-04-20 15:49:04 +0200

    dodanie ramki głównej

--- gameproject/src/main/java/com/dxc/gameproject/gui/BoardPanel.java
+++ gameproject/src/main/java/com/dxc/gameproject/gui/BoardPanel.java
@@ -2,9 +2,7 @@ package com.dxc.gameproject.gui;
 
 import javax.swing.JPanel;
 
-public class BoardPanel extends JPanel{
-
-    //test2
+public class BoardPanel{
 
 }
 
--- gameproject/src/main/java/com/dxc/gameproject/gui/GameFrame.java
+++ gameproject/src/main/java/com/dxc/gameproject/gui/GameFrame.java
@@ -1,42 +1,13 @@
 package com.dxc.gameproject.gui;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
 
 public class GameFrame {
-    private JFrame frame;
-    private JPanel backgroundPanel;
-    private JPanel mainPanel;
-    private JButton startButton;
-    private JButton moveUpButton;
-    private JButton moveDownButton;
-    //private JButton moveLeftButton;
-    //private JButton moveRightButton;
 
     public void createMainGUI() {
-        frame = new JFrame("Janusz na Wakacjach");
-        backgroundPanel = new JPanel();
-        mainPanel = new JPanel();
-        backgroundPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-
-
-        startButton = new JButton("Start");
-        moveUpButton = new JButton("Up");
-        moveDownButton = new JButton("Down");
-        //moveLeftButton = new JButton("Left");
-        //moveRightButton = new JButton("Right");
-
-        mainPanel.add(moveUpButton);
-        mainPanel.add(moveDownButton);
-        //mainPanel.add(moveRightButton);
-        //panel.add(moveLeftButton);
-
-        frame.getContentPane().add(BorderLayout.EAST, mainPanel);
-
+        JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(700,700);
         frame.setVisible(true);
     }
-
 }
