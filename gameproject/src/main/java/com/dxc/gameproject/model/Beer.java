e60bdfef82828016585fec35314a6e1151bd04d3
kuzniar <miron.kuzniar@gmail.com>
2017-04-26 15:35:18 +0200

    added class description

--- gameproject/src/main/java/com/dxc/gameproject/model/Beer.java
+++ gameproject/src/main/java/com/dxc/gameproject/model/Beer.java
@@ -2,6 +2,7 @@ package com.dxc.gameproject.model;
 
 /**
  * Created by kuzniar on 2017-04-26.
+ * Beer is an object placed on map. Beer position during one level is constant.
  */
 public class Beer {
     int beerPositionX;
