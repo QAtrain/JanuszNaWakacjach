61afb2a86eadb9a1b57954bd29c9f68d886128fb
kuzniar <miron.kuzniar@gmail.com>
2017-04-26 15:31:02 +0200

    added moethods & instances

--- gameproject/src/main/java/com/dxc/gameproject/model/Beer.java
+++ gameproject/src/main/java/com/dxc/gameproject/model/Beer.java
@@ -4,4 +4,10 @@ package com.dxc.gameproject.model;
  * Created by kuzniar on 2017-04-26.
  */
 public class Beer {
+    int beerPositionX;
+    int beerPositionY;
+
+    public void setBeerPosition() {
+
+    };
 }
