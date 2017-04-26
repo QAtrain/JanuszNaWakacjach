0926b2bb5cbff4546247b7fa78c22a92da2c3d72
Anna S <asikorska@hpe.com>
2017-04-26 15:35:28 +0200

    Added Game, Map and GameController

---
+++ gameproject/src/main/java/com/dxc/gameproject/model/Game.java
@@ -0,0 +1,8 @@
+package com.dxc.gameproject.model;
+
+/**
+ * This is the game. It contains map, timer, janusz and beer.
+ */
+public class Game {
+
+}
---
+++ gameproject/src/main/java/com/dxc/gameproject/model/Map.java
@@ -0,0 +1,19 @@
+package com.dxc.gameproject.model;
+
+/**
+ * Map resembles x / y grid on which Janusz and Beer are positioned.
+ */
+public class Map {
+    static final int MIN_LENGTH = 4;
+    static final int MAX_LENGTH = 4;
+
+    int length;
+    int width;
+
+    /** This method initializes map. */
+    void createMap() {
+        //TODO set length
+        //TODO set width
+    }
+
+}
