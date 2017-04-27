d997993d2779533ab5e7aeb90f259a631c22d56b
Ratay23 <ratay23@gmail.com>
2017-04-27 12:43:17 +0200

    Ania's version of Map class

--- gameproject/src/main/java/com/dxc/gameproject/model/Map.java
+++ gameproject/src/main/java/com/dxc/gameproject/model/Map.java
@@ -4,16 +4,27 @@ package com.dxc.gameproject.model;
  * Map resembles x / y grid on which Janusz and Beer are positioned.
  */
 public class Map {
-    static final int MIN_LENGTH = 4;
-    static final int MAX_LENGTH = 4;
 
+    /** Minimal grid length. */
+    static final int LENGTH_MIN = 4;
+    /** Maximum grid's length */
+    static final int LENGTH_MAX = 16;
+    /** Minimal grid width. */
+    static final int WIDTH_MIN = 4;
+    /** Maximum grid's width */
+    static final int WIDTH_MAX = 16;
+
+    /** Map's current length. */
     int length;
+    /** Map's current width. */
     int width;
 
     /** This method initializes map. */
     void createMap() {
+        System.out.println("Creating map...");
         //TODO set length
         //TODO set width
+        System.out.println("Map created.");
     }
 
 }
