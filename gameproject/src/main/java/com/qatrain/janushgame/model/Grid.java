b963e63be7c8b447a695e0be79b7472e2fd61f55
kuzniar <miron.kuzniar@gmail.com>
2017-05-12 12:54:45 +0200

    Changed Grid code after review.
    Changed all methods to "return" methods - now we can create unit tests.
    Grid initialization in constructor.
    Changed values type.
    Deleted example AppTest class.

--- gameproject/src/main/java/com/qatrain/janushgame/model/Grid.java
+++ gameproject/src/main/java/com/qatrain/janushgame/model/Grid.java
@@ -1,74 +1,93 @@
 package com.qatrain.janushgame.model;
 
-import java.util.ArrayList;
-
 /**
  * Grid resembles x / y grid on which Janusz and Beer are positioned.
  */
 public class Grid {
-    /**
-     * Setting random rows and columns value (3 < ROWS, COLUMNS < 16)
-     */
-    public static final int ROWS = (int) (Math.random() * 13) + 3;
-    public static final int COLUMNS = (int) (Math.random() * 13) + 3;
+
+    public int rows;
+    public int columns;
+    public int[][] board;
 
     /**
      * Static values for SWITCH case
      */
     public static final int EMPTY = 0;
-    public static final int JANUSZ = 1;
+    public static final int JANUSH = 1;
     public static final int BEER = 2;
 
-    public static final int[][] board = new int[ROWS][COLUMNS];
+    /**
+     * Setting static values which represent MAX and MIN quantity for ROWS and COLUMS in Grid
+     */
+    public static final int ROWS_MIN = 3;
+    public static final int ROWS_MAX = 13; //in fact max value of ROWS is 16, but we have to set 13 to create random algorithm which can create correct grid
+
+    public static final int COLUMNS_MIN = 3;
+    public static final int COLUMNS_MAX = 13; //the same situation as above
+
+    /**
+     * Setting algorithm which can create random Grid using static values inside constructor
+     */
+    public Grid() {
+        rows = (int) (Math.random() * ROWS_MAX) + ROWS_MIN; //Random function from Math class returns the value between 0 and 1, so we have to use algorithm to create Grid with 3 <= size <= 16
+        columns = (int) (Math.random() * COLUMNS_MAX) + COLUMNS_MIN; //The same as above
+        board = new int[rows][columns];
+    }
 
     /**
      * This method initializes grid.
      */
-    public static void createGrid() {
+    public String createGrid() {
         System.out.println("Creating grid...");
+        System.out.println(drawTable());
+        System.out.println("Grid created.");
+        return drawTable();
+    }
 
-        System.out.println();
-        for (int row = 0; row < ROWS; ++row) {
-            drawRow(board[row]);
-            if (row != ROWS - 1) {
-                for (int columns = 0; columns < COLUMNS; ++columns) {
-                    System.out.print("--"); //print horizontal partition //
+    /**
+     * This method draws Table
+     */
+    public String drawTable() {
+        String printout = "";
+        for (int row = 0; row < rows; ++row) {
+            printout += drawRow(board[row]);
+            if (row != rows - 1) {
+                for (int columns = 0; columns < this.columns; ++columns) {
+                    printout += "--"; //print horizontal partition
                 }
-                System.out.println();
+                printout += "\n";
             }
         }
-        System.out.println();
-        System.out.println("Grid created.");
+        printout += "\n";
+        return printout;
     }
 
     /**
      * This method draws single row
      */
-    private static void drawRow(int[] ints) {
-        for (int column = 0; column < COLUMNS; ++column) {
-            printCell(ints[column]); //print each of the cells
-            System.out.print("|"); //print vertical partition
+    private String drawRow(int[] ints) {
+        String printout = "";
+        for (int column = 0; column < columns; ++column) {
+            printout += printCell(ints[column]); //print each of the cells
+            printout += ("|"); //print vertical partition
         }
-        System.out.println();
+        printout += "\n";
+        return printout;
     }
 
     /**
-     * This method print a cell with a specified 'content' (not finished yet, for future purposes - DO NOT CHECK!)
+     * This method print a cell with a specified 'content' (for now - it's using only EMPTY case, )
      */
-    public static void printCell(int content) {
+    private String printCell(int content) {
         switch (content) {
             case EMPTY:
-                System.out.print(" "); //for now it will always get this case and generate empty board
-                break;
-            case JANUSZ:
-                System.out.print(" O ");
-                break;
+                return " "; //for now it will always get this case and generate empty board
+            case JANUSH:
+                return " O ";
             case BEER:
-                System.out.print(" X ");
-                break;
+                return " X ";
             default:
-                System.out.println("_");
-                break;
+                return "_";
         }
     }
 }
