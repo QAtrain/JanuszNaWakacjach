45bb3ca4d382adedfbbc9811a39db86b8137ec96
Aleksandra Gumkowska <aleksandra.gumkowska@hpe.com>
2017-06-07 09:26:04 +0200

    Adding test data for MovesTest

--- gameproject/src/test/java/com/qatrain/janushgame/model/MovesTest.java
+++ gameproject/src/test/java/com/qatrain/janushgame/model/MovesTest.java
@@ -2,7 +2,9 @@ package com.qatrain.janushgame.model;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by gumkowsk on 07-Jun-17.
@@ -10,9 +12,9 @@ import static org.junit.Assert.assertFalse;
 public class MovesTest {
 
     @Test
-    public void isOnReturnsFalseWhenObjectsHaveDifferentPosition_randomPosition() {
+    public void janushMovesLeft() {
         Janush janusz = new Janush();
-        Beer beer = new Beer();
+        //Beer beer = new Beer();
 
         //do not change both of these to negative numbers, because test will fail randomly!
         janusz.positionY = 4;
@@ -20,16 +22,18 @@ public class MovesTest {
 
         //random position from (0 - janusz.positionY)-1 will always yield different position than janusz's Y position (lower number)
         //unless someone changes janusz's positions above to negative numbers (with minus sign) - then this test will be WRONG
-        beer.positionY = (int) (Math.random() * (janusz.positionY - 1));
-        beer.positionX = (int) (Math.random() * (janusz.positionX - 1));
+        //beer.positionY = (int) (Math.random() * (janusz.positionY - 1));
+        //beer.positionX = (int) (Math.random() * (janusz.positionX - 1));
+
+        janusz.moveDown();
 
         System.out.println(janusz);
-        System.out.println(beer);
 
-        assertFalse("Janusz does not have same position as beer",
+
+        //assertTrue("Janush moved left his position is Y=4; X=4",
                 janusz.isOn(beer));
 
-        assertFalse("Beer does not have same position as janusz",
+        //assertFalse("Beer does not have same position as janusz",
                 beer.isOn(janusz));
     }
 }
