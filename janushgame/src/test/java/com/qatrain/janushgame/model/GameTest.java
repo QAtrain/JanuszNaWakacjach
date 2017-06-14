cbfb42b8c795d8d26ece6a53441bdc78d48961e0
kuzniar <miron.kuzniar@gmail.com>
2017-06-14 14:36:44 +0200

    Experiments

---
+++ janushgame/src/test/java/com/qatrain/janushgame/model/GameTest.java
@@ -0,0 +1,16 @@
+package com.qatrain.janushgame.model;
+
+import com.qatrain.janushgame.Game;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GameTest {
+
+    @Test
+    public void shouldGetNoLives() {
+        Game game = new Game();
+        int getLives = game.Start();
+        int expectedLives = 0;
+        Assert.assertEquals(expectedLives, getLives);
+    }
+}
